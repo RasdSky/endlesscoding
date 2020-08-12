40ceed53011648f1dad0696faf6e8845fedef224
rasdsky <rasdsky@qq.com>
2020-08-12 18:10:52 +0800

    feat:新增项目

---
+++ extension.js
@@ -0,0 +1,44 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "endlesscoding" is now active!');
+
+	let startGame = vscode.commands.registerCommand('endlesscoding.startgame', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		console.info('endlesscoding, Game Start!')
+	});
+
+	context.subscriptions.push(startGame);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
+
+
+	// "views": {
+	// 	"PlayerProperties": [
+	// 		{
+	// 			"id": "Attack",
+	// 			"name": "攻击"
+	// 		}
+	// 	]
+	// }
