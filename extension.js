1592663436c1d31acd9182244f57aebad24f08bd
rasdsky <rasdsky@qq.com>
2020-08-28 16:01:24 +0800

    add end

--- extension.js
+++ extension.js
@@ -9,7 +9,6 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "endlesscoding" is now active!');
